31a6deec68d86738c329dac9a19a0d79c37d6bd4
Hridoy Banik <hridoy.banik71@gmail.com>
2022-09-06 12:32:34 +0600

    initial setup client server

---
+++ server/app.js
@@ -0,0 +1,7 @@
+// express
+const express = require('express');
+const app = express();
+// database
+app.use(express.json());
+
+module.exports = app;
---
+++ server/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const app = require('./app');
+// database
+const connectDB = require('./db/connect');
+const port = process.env.PORT || 5000;
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL);
+        app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
